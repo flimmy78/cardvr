bd98da3c7b650cad51b56d72a68d578061fd3c7e
kochetkov.anton <kochetkov.anton@c3cc92ae-172b-99f5-1f7c-836cdcab7f91>
2010-08-22 20:54:14 +0000

    multi-execution protection

--- CarDVR/Program.cs
+++ CarDVR/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace CarDVR
 {
@@ -19,6 +20,15 @@ namespace CarDVR
         [STAThread]
         static void Main()
         {
+            int processCount = 0;
+
+            foreach (Process p in Process.GetProcesses())
+                if (p.ProcessName == Process.GetCurrentProcess().ProcessName)
+                {
+                    if (++processCount == 2)
+                        return;
+                }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new MyAppContext());
